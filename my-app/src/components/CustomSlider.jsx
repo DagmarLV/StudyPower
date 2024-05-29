c27fb42b29401851fed5b48efab6c183ccbabf22
Dagmar Lezama V. <dagmar.lezama.v@uni.pe>
2024-05-28 21:47:00 -0500

    fixing summary page desing

---
+++ my-app/src/components/CustomSlider.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+function valuetext(value) {
+    return `${value}°C`;
+}
+
+
+export default function DiscreteSlider() {
+  return (
+    <Box sx={{ width: 400, color:"success" }}>
+      <Slider
+        aria-label="Temperature"
+        defaultValue={30}
+        getAriaValueText={valuetext}
+        valueLabelDisplay="auto"
+        shiftStep={50}
+        step={10}
+        marks
+        min={10}
+        max={100}
+        color="principal"
+        
+      />
+      
+    </Box>
+  );
+}
