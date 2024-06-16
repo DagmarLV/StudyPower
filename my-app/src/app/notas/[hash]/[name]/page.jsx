24549a0e7c507894f9417b29d05f001649665a98
chandler-pc <chandler.perez.c@uni.pe>
2024-06-15 20:28:38 -0500

    Adding Voice To Text

--- my-app/src/app/notas/[hash]/[name]/page.jsx
+++ my-app/src/app/notas/[hash]/[name]/page.jsx
@@ -1,7 +1,18 @@
 'use client';
+import 'regenerator-runtime/runtime';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import createSpeechServicesPonyfill from 'web-speech-cognitive-services';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
+const SUBSCRIPTION_KEY = process.env.NEXT_PUBLIC_API_AZURE_VOICE;
+const REGION = 'eastus';
+const { SpeechRecognition: AzureSpeechRecognition } = createSpeechServicesPonyfill({
+    credentials: {
+      region: REGION,
+      subscriptionKey: SUBSCRIPTION_KEY,
+    }
+  });
 const NoteViewer = () => {
     const params = useParams();
     const [titleLabel, setTitleLabel] = useState("");
@@ -17,7 +28,7 @@ const NoteViewer = () => {
             });
         setNoteLabel(params.name);
     }, []);
-
+    const [isRecording, setIsRecording] = useState(false);
     const onSubmit = async (event) => {
         event.preventDefault()
 
@@ -36,11 +47,35 @@ const NoteViewer = () => {
         setDescription(response.description);
     }
 
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
+    const OnSpeechRecording = () => {
+        if (!browserSupportsSpeechRecognition) {
+            alert('Your browser does not support speech recognition software! Try Chrome desktop, maybe?');
+            return;
+        }
+        if (isRecording) {
+            SpeechRecognition.abortListening();
+            setIsRecording(false);
+            setDescription(transcript);
+            resetTranscript();
+            alert('Grabación finalizada');
+        } else {
+            SpeechRecognition.applyPolyfill(AzureSpeechRecognition);
+            SpeechRecognition.startListening({ language: 'es-ES'});
+            setIsRecording(true);
+        }
+    }
     return (
         <section className="container mx-auto flex flex-col md:gap-6 gap-4 p-4 md:ml-16 w-auto">
             <div className='md:w-2/3 mt-10 border-b-2 border-black/50 pb-4'>Bienvenido a tus apuntes</div>
             <div className=''>Inicio &gt; Apuntes &gt; {titleLabel} &gt; {noteLabel}</div>
             <h1>{noteLabel}</h1>
+            <button onClick={OnSpeechRecording} className='bg-gray-800 text-white py-2 px-4 rounded-full shadow w-24'>{listening? "Grabando..." : "Voz a texto"}</button>
             <form onSubmit={onSubmit}>
                 <textarea name="description" defaultValue={description} className='border-2 border-black/50 p-2 w-full h-96' >
                 </textarea>
